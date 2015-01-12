cd7b1a922e856edc842ffeec284b1b91c39da3a1
achintar <atry09@yahoo.com>
2015-01-12 17:23:07 +0530

    LIST OF BOOKS

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -16,9 +16,10 @@ public class Library {
 
     public void getBookList() {
         int i;
-        for (i = 0; i < 4; i++) {
-            book[i] = new Book("Black Lotus", "Satyaraj Das", "2007", "availbale");
-        }
+            book[0] = new Book("Black Lotus", "Satyaraj Das", "2007", "availbale");
+            book[1] = new Book("Harry Potter", "J.K Rowling", "1998", "availbale");
+            book[2] = new Book("Journey Home", "Richard Jones", "2005", "availbale");
+            book[3] = new Book("Let Us C", "Yashwant K", "2010", "availbale");
     }
 
     public void showBookList(PrintStream printStream) {
